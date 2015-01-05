e3861c778bfb316c7aedcca91191fa8197ce4f7d
Fabio Marzocca <marzoccafabio@gmail.com>
2015-01-05 18:36:31 +0100

    inizio a scrivere

--- display-site-numbers.php
+++ display-site-numbers.php
@@ -29,3 +29,68 @@
 
 defined('ABSPATH') or die("No script kiddies please!");
 define('DSN_DIR', dirname(__FILE__));
+
+
+class display_site_numbers extends WP_Widget {
+
+		function display_site_numbers() {
+			parent::WP_Widget(false, $name = __('Display Site Numbers', 'display-site-numbers') );
+		}
+
+
+	// Creating widget front-end
+	// This is where the action happens
+	public function widget( $args, $instance ) {
+		$title = apply_filters( 'widget_title', $instance['title'] );
+		// before and after widget arguments are defined by themes
+		echo $args['before_widget'];
+		if ( ! empty( $title ) )
+		echo $args['before_title'] . $title . $args['after_title'];
+
+		// This is where you run the code and display the output
+		echo __( 'Hello, World!', 'display-site-numbers' );
+		echo $args['after_widget'];
+	}
+		
+	// Widget Backend 
+	public function form( $instance ) {
+		if ( isset( $instance[ 'title' ] ) ) {
+		$title = $instance[ 'title' ];
+		}
+		else {
+		$title = __( 'New title', 'display-site-numbers' );
+		}
+		// Widget admin form
+		?>
+<p>
+<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
+<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+</p>
+<?php 
+	}
+	
+	// Updating widget replacing old instances with new
+	public function update( $new_instance, $old_instance ) {
+		$instance = array();
+		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( 	$new_instance['title'] ) : '';
+		return $instance;
+	}
+
+} // Class display_site_numbers ends here
+
+// Register and load the widget
+function DSN_load_widget() {
+	register_widget( 'display_site_numbers' );
+}
+add_action( 'widgets_init', 'DSN_load_widget' );
+
+
+
+/* Add CSS */	
+function DSN_css(){
+		wp_register_style( 'DSN_css', plugins_url( 'DSN.css' , __FILE__ ) );
+		wp_enqueue_style( 'DSN_css' );
+	} // function
+add_action( 'wp_enqueue_scripts', 'DSN_css' );
+
+?>
