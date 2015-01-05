ea32c7dd0281c2125010e383b830c3d3f23c6382
Fabio Marzocca <marzoccafabio@gmail.com>
2015-01-05 18:15:13 +0100

    first commit

---
+++ display-site-numbers.php
@@ -0,0 +1,31 @@
+<?php
+/*
+	Plugin Name: Display Site Numbers
+	Plugin URI: https://github.com/fmarzocca/display-site-numbers
+	Description: A widget to display all relevant site content numbers
+	Version: 0.5
+	Author: Fabio Marzocca
+	Author URI: http://www.marzocca.net
+	Text Domain:   display-site-numbers
+  	Domain Path:   /languages/
+	License: GPL2
+	
+	Copyright 2015  by Fabio Marzocca  (email : marzoccafabio@gmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
+defined('ABSPATH') or die("No script kiddies please!");
+define('DSN_DIR', dirname(__FILE__));
---
+++ readme.txt
@@ -0,0 +1,44 @@
+=== Display Site Numbers  ===
+=======================
+
+
+Contributors: fmarzocca
+Donate link: TBD
+Tags: widget, count posts, count categories, widget
+Requires at least: 4.0.1
+Tested up to: 4.1
+Stable tag: 0.5
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Widget to display all relevant site content numbers
+
+== Description ==
+
+The widget will show (and automatically update) the following site-content counters:
+
+* Categories count
+* Posts count
+* Images count
+* Tags count
+* Comments count
+
+In the admin backend, user can select what counter(s) to display.
+
+
+
+== Installation ==
+
+1. Upload `display-site-numbers` folder to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Place the widget 
+
+== Frequently Asked Questions ==
+
+= Can I customize the style in the widget? =
+   Yes, all elements are included in *DSN-wrapper* div. Left items are in the .item class, while counters are in .count class. You can override them in your child theme style.css
+
+== Changelog ==
+
+= 0.5 =
+* First working version
